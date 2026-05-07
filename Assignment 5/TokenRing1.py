66201821e6e0082027a5d46010889967d260128f
Srirang.Kalantri <srirang.kalantri@mendix.com>
2026-05-08 00:20:57 +0530

    n

---
+++ Assignment 5/TokenRing1.py
@@ -0,0 +1,64 @@
+import socket
+import threading
+import time
+
+# CHANGE IP ADDRESS
+next_ip = ""
+
+# CHANGE PORTS
+my_port = 8080
+next_port = 8081
+
+# True only for first machine
+create_token = True
+
+
+def critical_section():
+    print("Entering Critical Section")
+    time.sleep(5)
+    print("Exiting Critical Section")
+
+
+def pass_token():
+    s = socket.socket()
+    
+    while True:
+        try:
+            s.connect((next_ip, next_port))
+            break
+        except:
+            time.sleep(1)
+
+    s.send("TOKEN".encode())
+    print("Token Passed")
+
+    s.close()
+
+
+def receive_token():
+    server = socket.socket()
+
+    server.bind(("", my_port))
+    server.listen(1)
+
+    print("Waiting for Token...")
+
+    while True:
+        conn, addr = server.accept()
+
+        token = conn.recv(1024).decode()
+
+        if token == "TOKEN":
+            print("Token Received")
+
+            critical_section()
+
+            pass_token()
+
+        conn.close()
+
+if create_token:
+    time.sleep(5)
+    pass_token()
+
+threading.Thread(target=receive_token).start()
